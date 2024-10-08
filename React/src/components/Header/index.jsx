d3760fb53dc85586421b524bb47a2028f27b3afa
marikae23 <maryeguchi6@gmail.com>
2024-10-07 21:32:15 -0300

    Funcionou

---
+++ React/src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+import './style.css'
+
+export default function Header () {
+    return (
+        <>
+        <header>
+        <h1>Logo</h1>
+            <div id='header-links'>
+                <a href=''>Contato</a><br></br>
+                <a href=''>Projetos</a>
+            </div>
+        </header>
+        </>
+    )
+}
